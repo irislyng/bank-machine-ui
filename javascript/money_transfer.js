fdb12fecaffa1be6dd97a684baa687f915da3304
Sadisha Galappatti <sadishag@gmail.com>
2016-11-08 00:24:50 -0500

    Added history tracking on money_transfers

--- javascript/money_transfer.js
+++ javascript/money_transfer.js
@@ -8,12 +8,22 @@ var savingsBalance = getSavingsBalance();
 
 
 
-function setAccountBalance(value, acntType) {
+function setAccountBalance(value, acntType, fundsOut, diff) {
 	// set the balance of the account and update localStorage
 
 	// account = [ {"pin": 1234, "accounts": { "chequing": { "balance": 7000 }, "savings": { "balance": 4000 }}, "name": "John Doe", "history": [] }]
 	user["accounts"][acntType]["balance"] = value;
-	console.log("Account " + acntType + "updated: " + value);
+	console.log("Account " + acntType + " updated: " + value);
+
+	var date = new Date();
+
+
+	// fundsOut means that money was taken out of the account. 
+	if(fundsOut) {
+		user["history"].push(JSON.stringify({ "account": acntType, "fundsOut": fundsOut, "amount": diff, "curr_balance": value, "date": date.toString() }));
+	} else {
+		user["history"].push(JSON.stringify({ "account": acntType, "fundsOut": fundsOut, "amount": diff, "curr_balance": value, "date": date.toString() }));
+	}
 
 	localStorage.setItem(currentAccount, JSON.stringify(user));
 	chequingBalance = getChequingBalance();
@@ -25,10 +35,10 @@ function withdraw(fromAcnt, amount) {
 
 	if(fromAcnt == "chequing") {
 		chequingBalance = chequingBalance - amount;
-		setAccountBalance(chequingBalance, fromAcnt);
+		setAccountBalance(chequingBalance, fromAcnt, true, amount);
 	} else if(fromAcnt == "savings"){
 		savingsBalance = savingsBalance - amount;
-		setAccountBalance(savingsBalance, fromAcnt);
+		setAccountBalance(savingsBalance, fromAcnt,  true, amount);
 	} else {
 		// this will never be hit unless you're a hacker
 		console.log("Not An Account");
@@ -42,10 +52,10 @@ function deposit(toAcnt, amount) {
 
 	if(toAcnt == "chequing") {
 		chequingBalance = chequingBalance + amount;
-		setAccountBalance(chequingBalance, toAcnt);
+		setAccountBalance(chequingBalance, toAcnt, false, amount);
 	} else if(toAcnt == "savings") {
 		savingsBalance = savingsBalance + amount;
-		setAccountBalance(savingsBalance, toAcnt);
+		setAccountBalance(savingsBalance, toAcnt, false, amount);
 	} else {
 		// this will never be hit unless you're a hacker
 		console.log("Not An Account");
@@ -66,15 +76,15 @@ function transferFund(fromAcnt, toAcnt, amount) {
 		chequingBalance = chequingBalance - amount;
 		savingsBalance = savingsBalance + amount;
 
-		setAccountBalance(chequingBalance, fromAcnt);
-		setAccountBalance(savingsBalance, toAcnt);
+		setAccountBalance(chequingBalance, fromAcnt, true, amount);
+		setAccountBalance(savingsBalance, toAcnt, false, amount);
 
 	} else if(fromAcnt == "savings" && toAcnt == "chequing") {
 		savingsBalance = savingsBalance - amount;
 		chequingBalance = chequingBalance + amount;
 
-		setAccountBalance(savingsBalance, fromAcnt);
-		setAccountBalance(chequingBalance, toAcnt);
+		setAccountBalance(savingsBalance, fromAcnt, true, amount);
+		setAccountBalance(chequingBalance, toAcnt, false, amount);
 
 	} else {
 		// Shouldn't ever get hit unless you're trying to hack
