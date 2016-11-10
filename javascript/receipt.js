c93f2f256bbcbb1a88261c8775ec85e372e017d1
Mario Machado <mario.machado94@gmail.com>
2016-11-09 19:10:32 -0500

    Removed console logs and unused vars

--- javascript/receipt.js
+++ javascript/receipt.js
@@ -1,13 +1,8 @@
-chequingBalance = user["accounts"]["chequing"]["balance"];
-savingsBalance = user["accounts"]["savings"]["balance"];
-
 function transferReceipt (params) {
 	// TODO: Implement function
 }
 
 function withdrawalReceipt(account, amount) {
-	console.log("in withdrawalReceipt() with account: " + account + " and amount: " + amount);
-
 	var user = getLocalUser();
 	var balance = user["accounts"][account]["balance"];
 
