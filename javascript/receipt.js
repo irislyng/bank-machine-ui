09b2348d3100c9d99e39f94320e55b71fa1649b7
Mario Machado <mario.machado94@gmail.com>
2016-11-09 22:19:16 -0500

    Added receipt to deposits

--- javascript/receipt.js
+++ javascript/receipt.js
@@ -17,8 +17,19 @@ function withdrawalReceipt(account, amount) {
 	location.href="receipt.html"
 }
 
-function depositeReceipt(params) {
-	// TODO: Implement function
+function depositReceipt(account, amount) {
+	var user = getLocalUser();
+	var balance = user["accounts"][account]["balance"];
+
+	var receipt = {
+		type: "deposit",
+		balance: balance, 
+		amount: amount,
+		account: account
+	};
+
+	localStorage.setItem("receipt", JSON.stringify(receipt));
+	location.href="receipt.html"
 }
 
 function getLocalUser() {;
